0513631386c1afa0dd53c385d7f2c79924802131
Osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-01-25 16:02:57 +0100

    react-api

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App()
+{
+  
+  return (
+    <div className="App">
+      <button>Generate Cat Facts</button>
+      <p> </p>
+    </div>
+  );
+}
+
+export default App;
