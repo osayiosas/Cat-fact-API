80522fd3cc4d7bce9d8fbbe445b5636fef70a6d0
Osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-01-25 16:28:32 +0100

    add more function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 
 import './App.css';
+import Axios from "axios";
+import { useEffect, useState } from 'react';
+
+
 
 function App()
 {
+
+  const [catFact, setCatFact] = useState("")
+
+  Axios.get("https://catfact.ninja/fact").then((res) => {
+    setCatFact(res.data.fact);
+  });
+
+  useEffect(() => {});
+  
   
   return (
     <div className="App">
       <button>Generate Cat Facts</button>
-      <p> </p>
+      <p>{catFact} </p>
     </div>
   );
 }
